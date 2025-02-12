8ba7538bddc7da182a8bded30a019ca57f07d391
shrimp-18 <nehaarya1842008@gmail.com>
2025-02-12 23:53:50 +0530

    organized files

--- BG.gd
+++
@@ -1,7 +0,0 @@
-extends ParallaxBackground
-
-var scrollingspeed=100
-
-func _process(delta):
-	scroll_offset.x -=scrollingspeed*delta
-	
---
+++ scripts/BG.gd
@@ -0,0 +1,11 @@
+extends ParallaxBackground
+
+var scrollingspeed=100
+
+func _process(delta):
+	scroll_offset.x -=scrollingspeed*delta
+	
+
+
+func _on_quit_pressed():
+	get_tree().quit()
