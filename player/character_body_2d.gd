09ba27f5702daf91ee9faa840f2f99d17c1f7e9a
shrimp-18 <nehaarya1842008@gmail.com>
2025-02-14 01:50:41 +0530

    movement for fairy

--- player/character_body_2d.gd
+++ player/character_body_2d.gd
@@ -1,13 +1,17 @@
 extends CharacterBody2D
-const maxspeed=400
+
+const maxspeed=1000
 const accln=1500
 const friction=600
 
 var input=Vector2.ZERO
 
+func _physics_process(delta):
+	player_movement(delta)
+
 func get_input():
-	input.x=int(Input.is_action_pressed("vi.right"))-int(Input.is_action_pressed("vi.left"))
-	input.y=int(Input.is_action_pressed("vi.down"))-int(Input.is_action_pressed("vi.up"))
+	input.x= int(Input.is_action_pressed("ui_right"))-int(Input.is_action_pressed("ui_left"))
+	input.y= int(Input.is_action_pressed("ui_down"))-int(Input.is_action_pressed("ui_up"))
 	return input.normalized()
 
 func player_movement(delta):
