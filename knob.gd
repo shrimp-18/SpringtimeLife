d470907f2eafd5413915b31f352dbf4fffcbfcbc
shrimp-18 <nehaarya1842008@gmail.com>
2025-02-16 23:41:16 +0530

    fixed joystick

--- knob.gd
+++ knob.gd
@@ -1,28 +1,39 @@
 extends Sprite2D
+@onready var parent = $".."
 
 
-@onready var parent= $".."
 
-var pressing= false
+var pressing = false
 
-@export var maxLength=100
-@export var deadzone=5
+@export var maxLength = 50
+var deadzone = 15
 
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	
+	maxLength *= parent.scale.x
 
-
-func _process(_delta):
+func _process(delta):
 	if pressing:
-		if get_global_mouse_position().distance_to(parent.global_position)<= maxLength :
-			global_position=get_global_mouse_position()
+		if get_global_mouse_position().distance_to(parent.global_position) <= maxLength:
+			global_position = get_global_mouse_position()
+		else:
+			var angle = parent.global_position.angle_to_point(get_global_mouse_position())
+			global_position.x = parent.global_position.x + cos(angle)*maxLength
+			global_position.y = parent.global_position.y + sin(angle)*maxLength
+		calculateVector()
 	else:
-		global_position=lerp(global_position,parent.global_position,_delta*30)
-
-
-func _on_button_button_down() :
-	pressing=true
+		global_position = lerp(global_position, parent.global_position, delta*50)
+		parent.posVector = Vector2(0,0)
+		
+func calculateVector():
+	if abs((global_position.x - parent.global_position.x)) >= deadzone:
+		parent.posVector.x = (global_position.x - parent.global_position.x)/maxLength
+	if abs((global_position.y - parent.global_position.y)) >= deadzone:
+		parent.posVector.y = (global_position.y - parent.global_position.y)/maxLength
+		
+func _on_button_button_down():
+	pressing = true
 
 
 func _on_button_button_up():
-	pressing=false
+	pressing = false
