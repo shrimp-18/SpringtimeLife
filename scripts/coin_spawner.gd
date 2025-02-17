8d0df02729ca007190e44a0b246143fe552d36e9
shrimp-18 <nehaarya1842008@gmail.com>
2025-02-18 01:06:12 +0530

    added coin spawner

---
+++ scripts/coin_spawner.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+@onready var game= get_node("/root/game")
+
+var coinscn :=preload("res://scenes/coin.tscn")
+var spawnpts:= []
+
+func _ready():
+	
+	for i in get_children():
+		if i is Marker2D:
+			spawnpts.append(i)
+
+
+
+	
+
+func _on_timer_timeout():
+	var spawn=spawnpts[randi()%spawnpts.size()]
+	var coin_spawner=coinscn.instantiate()
+	coin_spawner.position=spawn.position
+	game.add_child(coin_spawner)
