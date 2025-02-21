1ef6a51825727fe2f1edcf71fb5d8c836ac2524c
shrimp-18 <nehaarya1842008@gmail.com>
2025-02-22 03:18:47 +0530

    made avatarscn better

--- scripts/home.gd
+++ scripts/home.gd
@@ -10,7 +10,7 @@ func _on_playb_pressed():
 
 
 func _on_avatar_pressed():
-	get_tree().change_scene_to_file("res://carouselplayer.tscn")
+	get_tree().change_scene_to_file("res://scenes/avatarscn.tscn")
 
 
 func _on_texture_button_pressed():
