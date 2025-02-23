b617685f712f714116ee99f1fc6ed297b784448c
Tanmay Raj Krishna <tanmayrajkrishna@gmail.com>
2025-02-23 18:53:10 +0530

    add coin

--- scenes/fruit_ui.gd
+++ scenes/fruit_ui.gd
@@ -1,9 +1,8 @@
 extends Control
 
 @onready var label=$Label
-
 func _ready():
-	EventController.connect("coin_collected",on_event_coin_collected)
+	EventController.connect("fruit_collected",on_event_coin_collected)
 
 func on_event_coin_collected(value:int):
 	label.text=str(value)
--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,11 +1,12 @@
-extends CharacterBody2D
+extends Area2D
+
 @onready var anim=get_node("AnimatedSprite2D")
 @export var value:int=1
+
 func _ready():
 	anim.play("coin")
 
-
-func _on_area_2d_body_entered(body):
+func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		GameController.coin_collected(value)
 		self.queue_free()
