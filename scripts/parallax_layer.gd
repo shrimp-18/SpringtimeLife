897a433c278577051e8d9881781fb355271e8b64
shrimp-18 <nehaarya1842008@gmail.com>
2025-03-13 04:56:14 +0530

    parallax

--- scripts/parallax_layer.gd
+++ scripts/parallax_layer.gd
@@ -1,6 +1,6 @@
 extends ParallaxLayer
 
-@export var scrolling_speed: float = 100
+@export var scrolling_speed: float = 120
 
 func _process(delta: float) -> void:
 	motion_offset.x -= scrolling_speed * delta
