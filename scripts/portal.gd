be4f01c21144b41d6a07a558443aa7381d6f3fc4
shrimp-18 <nehaarya1842008@gmail.com>
2025-03-23 22:50:25 +0530

    changing levels using portal

--- scripts/portal.gd
+++ scripts/portal.gd
@@ -6,4 +6,12 @@ func _ready():
 
 func _on_body_entered(body):
 	if body is Player:
-		get_tree().change_scene_to_file("res://scenes/level2.tscn")
+		if get_tree().current_scene.scene_file_path == "res://scenes/game.tscn":
+			get_tree().change_scene_to_file("res://scenes/level2.tscn")
+			GameController.total_health=100
+		elif get_tree().current_scene.scene_file_path=="res://scenes/level2.tscn":
+			get_tree().change_scene_to_file("res://scenes/level3.tscn")
+			GameController.total_health=100
+		elif get_tree().current_scene.scene_file_path=="res://scenes/level3.tscn":
+			get_tree().change_scene_to_file("res://scenes/level4.tscn")
+			GameController.total_health=100
